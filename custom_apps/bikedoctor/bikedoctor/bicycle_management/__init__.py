7649b00dc0efe92c1d1c8a2c90831fa7b0ff62ea
nc-agency <info.ncagency@gmail.com>
2025-05-05 11:22:33 +0200

    Bicycle Management Modul Initialisierungsdatei hinzugefügt

---
+++ custom_apps/bikedoctor/bikedoctor/bicycle_management/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# bike.doctor ERP - Bicycle Management Module Initialization
+# 
+# Erstellt: 05.05.2025
+# Änderungen:
+# - Initiale Erstellung der Initialisierungsdatei für das Bicycle Management Modul
+
+from __future__ import unicode_literals
+from bikedoctor.bicycle_management.bicycle_management import (
+    on_sales_invoice_submit,
+    get_repair_history,
+    create_invoice,
+    update_bicycle_status
+)
